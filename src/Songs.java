5eb843ac3ac212af6d5f8c49055736f571ddaf80
ravleen29 <ravleenkaurspl@gmail.com>
2022-11-03 21:51:02 +0000

    removing a song

--- src/Songs.java
+++ src/Songs.java
@@ -1,20 +1,27 @@
 public class Songs {
-    private String Song_Name;
+    private String Song_Title;
     private String Artist_Name;
     private int Plays;
 
-    public Songs(String Song_Name, String Artist_Name, int Plays)
+    public Songs(String Song_Title, String Artist_Name, int Plays)
     {
-        this.Song_Name = Song_Name;
+        this.Song_Title = Song_Title;
         this.Artist_Name = Artist_Name;
         this.Plays = Plays;
     }
+
     public void print()
     {
-        System.out.print("Song: " + this.Song_Name);
+        System.out.print("Song: " + this.Song_Title);
         System.out.print(" Artist: " + this.Artist_Name );
         System.out.println(" Plays: " + this.Plays);
     
     }
+
+    public String getName(){
+        return this.Song_Title;
+    }
+
+    
     
 }
